fac363ecdf77d3979c74ddece43464f326b21a3e
some-sarkar <someshwarsarkar9851@gmail.com>
2025-05-18 00:39:59 +0530

    Initial commit

---
+++ src/test/java/com/banking/user_services/UserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.user_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
